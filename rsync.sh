ed2490df7cb0ae6dc826b83105b82d4977ddd4e5
Jelle Meeus <jellemeeus@hotmail.com>
2022-04-10 11:16:36 +0200

    change -v -r to -a -i

--- rsync.sh
+++ rsync.sh
@@ -3,6 +3,6 @@
 FILTER=${HOME}/scripts/cronjobs/filter_rsync.txt
 LOG_FILE=${HOME}/.local/log/rsync_${DRIVE}_${FILES}_${FREQUENCY}.log
 rm -f $LOG_FILE
-rsync -v -r --delete -f 'merge '${FILTER} \
+rsync -a -i --delete -f 'merge '${FILTER} \
 --log-file $LOG_FILE \
 /home/user/ /media/user/${DRIVE}/backups/${FREQUENCY}
